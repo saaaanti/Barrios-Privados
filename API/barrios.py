d06de9dabad005999ce1514a0c5e53b7b512a32e
Santiago Piñero <89141124+s-anti@users.noreply.github.com>
2023-06-21 22:09:47 -0300

    tablas, demo-datos a medias

--- API/barrios.py
+++ API/barrios.py
@@ -1,63 +1,80 @@
 import sqlite3 as sql
+import os
 
 
 class Barrios:
-    def __init__(self):
-        self.db = sql.connect("barrios.sqlite3")
-        self.cur = self.db.cursor()
+    def __init__(self, path: str):
+        self.conn = sql.connect(path)
+        self.cur = self.conn.cursor()
 
     def crearTablas(self):
         self.cur.execute(
             """CREATE TABLE IF NOT EXISTS Costos (
-				seguridad INTEGER,
-				kw INTEGER,
-				m3_agua INTEGER,
-				m3_gas INTEGER,
-				total_luz INTEGER,
-				mf_agua INTEGER,
-				mf_asf INTEGER,
-				vehiculos INTEGER,
-				m2_valor INTEGER
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+				seguridad FLOAT NOT NULL,
+				kw FLOAT NOT NULL,
+				m3_agua FLOAT NOT NULL,
+				m3_gas FLOAT NOT NULL,
+				total_luz FLOAT NOT NULL,
+				mf_agua FLOAT NOT NULL,
+				mf_asf FLOAT NOT NULL,
+				vehiculos FLOAT NOT NULL,
+				m2_valor FLOAT NOT NULL,
+                mes VARCHAR(6) NOT NULL UNIQUE CHECK(mes like '____-__')
 			)"""
         )
-        self.cur.execute(
-            """CREATE TABLE IF NOT EXISTS Consumos (
-				lot INTEGER,
-				man INTEGER,
-				nombre VARCHAR,
-				seguridad FLOAT,
-				luz FLOAT,
-				agua FLOAT,
-				gas FLOAT,
-				luz_publica FLOAT,
-				f_agua FLOAT, f_asf FLOAT, vehiculo FLOAT, terreno FLOAT
-			)"""
-        )
+
         self.cur.execute(
             """CREATE TABLE IF NOT EXISTS Propietarios (
-				nomape VARCHAR,
-				lote INTEGER,
-				manzana INTEGER,
-				fecha INTEGER,
-				sup_cub INTEGER,
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+				nombre VARCHAR NOT NULL,
+                apellido VARCHAR NOT NULL,
+				lot_id INTEGER,
+				fecha_compra DATE,
+				superficie_cub FLOAT,
 				habitantes INTEGER,
 				vehiculos INTEGER,
-				cons_luz INTEGER,
-				cons_agua INTEGER,
-				cons_gas INTEGER
+				cons_luz FLOAT,
+				cons_agua FLOAT,
+				cons_gas FLOAT,
+                FOREIGN KEY (lot_id) REFERENCES Lotes (id)
+			)"""
+        )
+        self.cur.execute(
+            """CREATE TABLE IF NOT EXISTS Manzanas (
+				id INTEGER PRIMARY KEY AUTOINCREMENT
 			)"""
         )
         self.cur.execute(
             """CREATE TABLE IF NOT EXISTS Lotes (
-				numero INTEGER,
-				manzana INTEGER,
-				m_frente INTEGER,
-				m_prof INTEGER,
-				luz INTEGER,
-				agua INTEGER,
-				asf INTEGER,
-				esq INTEGER,
-				lote_id INTEGER
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
+				manz_id INTEGER NOT NULL,
+				m_frente FLOAT NOT NULL,
+				m_prof FLOAT NOT NULL,
+				usa_luz BOOLEAN NOT NULL,
+				usa_agua BOOLEAN NOT NULL,
+				usa_asf BOOLEAN NOT NULL,
+				usa_esq BOOLEAN NOT NULL,
+                FOREIGN KEY (manz_id) REFERENCES Manzanas (id)
+			)"""
+        )
+        self.cur.execute(
+            """CREATE TABLE IF NOT EXISTS Consumos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                lot_id INTEGER NOT NULL,
+				prop_id INTEGER NOT NULL,
+                cost_id INTEGER NOT NULL,
+				seguridad FLOAT NOT NULL,
+				luz FLOAT NOT NULL,
+				agua FLOAT NOT NULL,
+				gas FLOAT NOT NULL,
+				luz_publica FLOAT NOT NULL,
+				f_agua FLOAT NOT NULL,
+                f_asf FLOAT NOT NULL,
+                vehiculo FLOAT NOT NULL,
+                FOREIGN KEY (lot_id) REFERENCES Lotes (id),
+                FOREIGN KEY (prop_id) REFERENCES Propietarios (id),
+                FOREIGN KEY (cost_id) REFERENCES Costos (id)
 			)"""
         )
 
@@ -68,36 +85,39 @@ class Barrios:
         return a
 
     def insertarMuestras(self):
-        if self.fetchDatos("SELECT * FROM Costos"):
+        if self.fetchDatos("SELECT * FROM Costos") == []:
+            print("??")
             self.cur.execute(
-                "INSERT INTO Costos VALUES (?,?,?,?,?,?,?,?,?)",
+                "INSERT INTO Costos VALUES (NULL,?,?,?,?,?,?,?,?,?,?)",
                 (
-                    30000,
-                    60,
-                    30,
-                    45,
-                    80000,
-                    40,
-                    70,
-                    10000,
-                    60000,
+                    30000.00,
+                    60.00,
+                    30.00,
+                    45.00,
+                    80000.00,
+                    40.00,
+                    70.00,
+                    10000.00,
+                    60000.00,
+                    "2023-06",
                 ),
             )
 
         prop_data = [
-            ["Perez Luis", 1, 1, 20160922, 600, 2, 1, 0, 2400, 1500],
-            ["Perez Luis", 2, 1, 20160922, 830, 0, 0, 1100, 0, 0],
-            ["Martinez Marcos", 1, 2, 20181204, 230, 4, 2, 1200, 200, 2300],
-            ["Gomez Lucas", 1, 3, 20220431, 0, 0, 0, 0, 1400, 0],
-            ["Perez Luis", 2, 2, 20210212, 0, 0, 0, 0, 0, 0],
-            ["Perez Luis", 2, 3, 20200512, 700, 5, 3, 0, 4230, 3400],
+            ["Perez", "Luis", 1, "2016-09-22", 600, 2, 1, 0, 2400, 1500],
+            ["Perez", "Luis", 2, "2016-09-22", 830, 0, 0, 1100, 0, 0],
+            ["Martinez", "Marcos", 1, "2018-12-04", 230, 4, 2, 1200, 200, 2300],
+            ["Gomez", "Lucas", 1, "2022-04-31", 0, 0, 0, 0, 1400, 0],
+            ["Perez", "Luis", 2, "2021-02-12", 0, 0, 0, 0, 0, 0],
+            ["Perez", "Luis", 2, "2020-05-12", 700, 5, 3, 0, 4230, 3400],
         ]
 
         if self.fetchDatos("SELECT * FROM Propietarios") == []:
             self.cur.executemany(
-                "INSERT INTO Propietarios VALUES (?,?,?,?,?,?,?,?,?,?)", prop_data
+                "INSERT INTO Propietarios VALUES (NULL,?,?,?,?,?,?,?,?,?,?)", prop_data
             )
-
+        self.conn.commit()
+        return
         lote_data = [
             [1, 1, 100, 120, 0, 1, 1, 1, 1],
             [2, 1, 110, 90, 1, 0, 1, 0, 2],
@@ -113,10 +133,18 @@ class Barrios:
                 "INSERT INTO Lotes VALUES (?,?,?,?,?,?,?,?,?)", lote_data
             )
 
+        self.conn.commit()
+
     def a(self):
         return "1023124"
 
 
-b = Barrios()
+path = "./barrios1.sqlite3"
+try:
+    os.remove(path)
+except:
+    print("Jjas")
+
+b = Barrios(path)
 b.crearTablas()
 b.insertarMuestras()
