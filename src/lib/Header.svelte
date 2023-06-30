39d60649ca50bec77e917263feafde9d51d4a224
s-anti <89141124+s-anti@users.noreply.github.com>
2023-06-30 11:30:38 -0300

    Argego los componentes que no estaban

---
+++ src/lib/Header.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let active = false;
+</script>
+
+<header class="bg-slate-800 flex p-5 gap-5 items-center">
+	<button
+		on:click={() => (active = !active)}
+		class="fa-solid fa-bars text-xl bg-red-400 p-1 px-2 rounded-full"
+	/>
+	<h1 class="text-xl h-min">Administración de Barrios Privados</h1>
+</header>
